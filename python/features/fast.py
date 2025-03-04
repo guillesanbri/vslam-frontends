0900eb001e52b428f6d5ccefeb7cfca5c878b53a
guillesanbri <guillesanbri@hotmail.com>
2025-03-04 14:30:16 +0100

    add implementation from scratch, some todos left

--- python/features/fast.py
+++ python/features/fast.py
@@ -1,5 +1,83 @@
 import cv2
 import time
+import numpy as np
+
+CIRCLE_OFFSETS = [
+    (0, -3),
+    (1, -3),
+    (2, -2),
+    (3, -1),
+    (3, 0),
+    (3, 1),
+    (2, 2),
+    (1, 3),
+    (0, 3),
+    (-1, 3),
+    (-2, 2),
+    (-3, 1),
+    (-3, 0),
+    (-3, -1),
+    (-2, -2),
+    (-1, -3),
+]
+
+
+def fast_check(img, x, y, thr) -> bool:
+    brighter = 0
+    darker = 0
+    center_value = img[y][x]
+
+    # compass coordinates
+    first_pass_indices = [1, 5, 9, 13]
+    for i in first_pass_indices:
+        i_value = img[y + CIRCLE_OFFSETS[i - 1][1]][x + CIRCLE_OFFSETS[i - 1][0]]
+        if i_value > center_value + thr:
+            brighter += 1
+        elif i_value < center_value - thr:
+            darker += 1
+
+    second_pass_indices = [2, 3, 4, 6, 7, 8, 10, 11, 12, 14, 15, 16]
+    if brighter >= 3 or darker >= 3:
+        # full check
+        for i in second_pass_indices:
+            i_value = img[y + CIRCLE_OFFSETS[i - 1][1]][x + CIRCLE_OFFSETS[i - 1][0]]
+            if i_value > center_value + thr:
+                brighter += 1
+            elif i_value < center_value - thr:
+                darker += 1
+
+        # TODO: Check for continuity, this is not correct
+        return brighter >= 9 or darker >= 9
+
+    return False
+
+
+def fast(img, thr=10):
+
+    print("=" * 80)
+    print("Scratch FAST Detector")
+    print("-" * 80)
+
+    kp = []
+    h, w = img.shape
+    img_float = img.astype(np.float32)
+    t0 = time.perf_counter()
+    for x in range(3, w - 3):
+        for y in range(3, h - 3):
+            if fast_check(img_float, x, y, thr):
+                kp.append((x, y))
+
+    # TODO: add nms
+
+    t1 = time.perf_counter()
+    print(f"Number of kp found (nms): {len(kp)}, took {(t1 - t0)*1000:.3f} ms")
+
+    kp = [
+        cv2.KeyPoint(_kp[0], _kp[1], size=7) for _kp in kp
+    ]  # size is diameter used in drawKeypoints
+    scratch_fast_nms = cv2.drawKeypoints(img, kp, None, (0, 255, 0))
+    cv2.imshow("Scratch FAST original img", img)
+    cv2.imshow("Scratch FAST with NMS", scratch_fast_nms)
 
 
 def opencv_fast(img):
@@ -40,7 +118,10 @@ def main():
         print("Img not found!")
         return
 
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
     opencv_fast(img)
+    fast(img)
     cv2.waitKey(0)
 
 
