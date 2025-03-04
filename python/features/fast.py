05001b8c93f2ac434bc563783f05e762bcaae982
guillesanbri <guillesanbri@hotmail.com>
2025-03-04 13:04:48 +0100

    add perf counters

--- python/features/fast.py
+++ python/features/fast.py
@@ -1,11 +1,17 @@
 import cv2
+import time
 
 
 def opencv_fast(img):
     fast = cv2.FastFeatureDetector_create()
+    print("=" * 80)
+    print("OpenCV FAST Detector")
+    print("-" * 80)
 
+    t0 = time.perf_counter()
     kp = fast.detect(img, None)
-    print(f"Number of kp found (nms): {len(kp)}")
+    t1 = time.perf_counter()
+    print(f"Number of kp found (nms): {len(kp)}, took {(t1 - t0)*1000:.3f} ms")
     opencv_fast_nms = cv2.drawKeypoints(img, kp, None, (0, 255, 0))
 
     sorted_kp = sorted(kp, key=lambda k: k.response, reverse=True)
@@ -13,10 +19,13 @@ def opencv_fast(img):
     opencv_fast_nms_top_100 = cv2.drawKeypoints(img, sorted_kp, None, (0, 255, 0))
 
     fast.setNonmaxSuppression(0)
+    t0 = time.perf_counter()
     kp = fast.detect(img, None)
-    print(f"Number of kp found (no nms): {len(kp)}")
+    t1 = time.perf_counter()
+    print(f"Number of kp found (no nms): {len(kp)}, took {(t1 - t0)*1000:.3f} ms")
     opencv_fast_no_nms = cv2.drawKeypoints(img, kp, None, (0, 255, 0))
 
+    print("=" * 80)
     cv2.imshow("OpenCV FAST original img", img)
     cv2.imshow("OpenCV FAST with NMS", opencv_fast_nms)
     cv2.imshow("OpenCV FAST (top 100)", opencv_fast_nms_top_100)
