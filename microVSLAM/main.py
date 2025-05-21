e54f4c2a07f8a54283cdd790fe8d341952a5e689
guillesanbri <guillesanbri@hotmail.com>
2025-05-21 02:02:38 +0200

    add dummy main to display input frame pairs

--- microVSLAM/main.py
+++ microVSLAM/main.py
@@ -1,5 +1,25 @@
+import cv2
+import numpy as np
+
+from sources.dataset_euroc_mav import EuRoCMAVDataset
+
+
 def main():
-    print(f"Hello from microvslam!")
+    dataset_path = "../sample_data/EuRoC_MAV/mav0"
+    source = EuRoCMAVDataset(dataset_path)
+
+    print("Playing sequences, press ESC to exit...")
+    for l, r in source:
+        if l.shape != r.shape:
+            print("Image shapes do not match!")
+            exit()
+
+        pair = np.hstack((l, r))
+        cv2.imshow("Stereo Pair", pair)
+
+        k = cv2.waitKey(30)
+        if k == 27:
+            break
 
 
 if __name__ == "__main__":
